910d986ffc219f64eb88c44902af851da1554e46
victorwilliam2 <j70043899@gmail.com>
2025-03-10 18:10:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 //Feel free to customize my bot informations to yours. Starting from this point
 //owmner v card
-global.ytname = "YT: ElitePro" //ur yt chanel name
+global.ytname = "YT: victor" //ur yt chanel name
 global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
+global.botname = 'victor william' //ur bot name
+global.ownernumber = '2348068530218' //ur owner number
 global.ownername = 'ELITE-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
@@ -19,8 +19,8 @@ global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
 global.creator = "2348109263390@s.whatsapp.net"
-global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.xprefix = '/' //set any prefix 
+global.premium = ["2348068530218"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
